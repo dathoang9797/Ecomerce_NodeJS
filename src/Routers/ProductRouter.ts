2d46d7be5ef4b0d3dc26fb40d12dd5f0225667fa
DESKTOP-9C48CIB\datho <dat>
2023-07-26 22:32:22 +0700

    build

--- src/Routers/ProductRouter.ts
+++ src/Routers/ProductRouter.ts
@@ -3,8 +3,36 @@ import { AppErrorHandling } from '@Utils/AppErrorHanding';
 import { catchError, } from '@Utils';
 import { IProduct, ProductModel } from '@Models/ProductModel';
 import { CategoryModel } from '@Models/CategoryModel';
+import multer from 'multer';
 import mongoose from 'mongoose';
 export const ProductRouter = express.Router();
+
+const FILE_TYPE_MAP: { [key: string]: string } = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg',
+};
+
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const isValid = FILE_TYPE_MAP[file.mimetype];
+        let uploadErr = new Error('Invalid image type');
+        if (isValid)
+            uploadErr = null
+
+        const basePath = `public/uploads`;
+        cb(uploadErr, basePath)
+    },
+    filename: function (req, file, cb) {
+        const extension = FILE_TYPE_MAP[file.mimetype];
+        const fileName = file.originalname.split(' ').join('-')
+        cb(null, `${fileName}-${Date.now()}.${extension}`)
+    }
+})
+
+const uploadOption = multer({ storage })
 
 ProductRouter.get('/', catchError(async (req: Request<{ id: string }, {}, IProduct, { categories: string }>, res: Response, next: NextFunction) => {
     // const productList = await ProductModel.find().select('name image -_id');
@@ -50,16 +78,23 @@ ProductRouter.get('/:id', catchError(async (req: Request<{ id: string }, {}, IPr
     res.status(200).send(productList);
 }))
 
-ProductRouter.post('/', catchError(async (req: Request<{}, {}, IProduct>, res: Response, next: NextFunction) => {
+ProductRouter.post('/', uploadOption.single('image'), catchError(async (req: Request<{}, {}, IProduct>, res: Response, next: NextFunction) => {
     const category = await CategoryModel.findById(req.body.category);
     if (!category)
         return next(new AppErrorHandling('Invalid Category', 400));
 
+    const file = req.file;
+
+    if (!file)
+        return next(new AppErrorHandling('No image in the request', 400));
+
+    const fileName = req.file.filename;
+    const basePath = `${req.protocol}://${req.get('host')}/static/uploads`;
     const product = new ProductModel({
         name: req.body.name,
         description: req.body.description,
         richDescription: req.body.richDescription,
-        image: req.body.image,
+        image: `${basePath}/${fileName}`,
         brand: req.body.brand,
         price: req.body.price,
         category: req.body.category,
@@ -76,7 +111,26 @@ ProductRouter.post('/', catchError(async (req: Request<{}, {}, IProduct>, res: R
     return res.status(200).json({ status: 'success', data: product });
 }))
 
-ProductRouter.put('/:id', catchError(async (req: Request<{ id: string }, {}, IProduct>, res: Response, next: NextFunction) => {
+ProductRouter.post('/gallery_images/:id', uploadOption.array('images', 10), catchError(async (req: Request<{ id: string }, {}, IProduct>, res: Response, next: NextFunction) => {
+    if (!mongoose.isValidObjectId(req.params.id))
+        return next(new AppErrorHandling('Invalid Product Id', 400));
+
+    const files = req.files;
+    const imagesPaths: string[] = [];
+    const basePath = `${req.protocol}://${req.get('host')}/static/uploads/`;
+    Array.isArray(files) && files.forEach(file => imagesPaths.push(`${basePath}${file.filename}`));
+    const product = await ProductModel.findByIdAndUpdate(req.params.id, {
+        images: imagesPaths,
+    }, { new: true })
+
+    if (!product)
+        return next(new AppErrorHandling('The product cannot be update!', 500));
+
+    return res.status(200).json({ status: 'success', product });
+}))
+
+
+ProductRouter.put('/:id', uploadOption.single('image'), catchError(async (req: Request<{ id: string }, {}, IProduct>, res: Response, next: NextFunction) => {
     if (!mongoose.isValidObjectId(req.params.id))
         return next(new AppErrorHandling('Invalid Product Id', 400));
 
@@ -84,11 +138,23 @@ ProductRouter.put('/:id', catchError(async (req: Request<{ id: string }, {}, IPr
     if (!category)
         return next(new AppErrorHandling('Invalid Category', 400));
 
+    const product = await ProductModel.findById(req.params.id);
+    if (!product)
+        return next(new AppErrorHandling('Invalid Product', 400));
+
+    const file = req.file;
+    let imagePath = product.image;
+    if (file) {
+        const fileName = req.file.filename;
+        const basePath = `${req.protocol}://${req.get('host')}/static/uploads/`;
+        imagePath = `${basePath}${fileName}`;
+    }
+
     const productUpdate = await ProductModel.findByIdAndUpdate(req.params.id, {
         name: req.body.name,
         description: req.body.description,
         richDescription: req.body.richDescription,
-        image: req.body.image,
+        image: imagePath,
         brand: req.body.brand,
         price: req.body.price,
         category: req.body.category,
--- src/Utils/Token.ts
+++ src/Utils/Token.ts
@@ -41,6 +41,7 @@ export const authJwt = () => {
     path: [
       { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
       { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+      { url: /\/static\/(.*)/, methods: ['GET', 'OPTIONS'] },
       '/api/v1/users/login',
       '/api/v1/users/register',
     ]
